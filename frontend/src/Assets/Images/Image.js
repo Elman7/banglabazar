328c20f7f07da197922afde3ab803e705b890dcd
Elman7 <elmannayeem1@gmail.com>
2024-07-16 19:12:59 +0600

    add img

--- frontend/src/Assets/Images/Image.js
+++ frontend/src/Assets/Images/Image.js
@@ -1,8 +1,8 @@
 export let payment = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838910/payment_mkel3w.jpg';
 export let profile = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838912/vecteezy_user-avatar-line-style__x94mjc.jpg';
-export let MaleCloth = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838911/Male-Cloth_fyyrzb.jpg';
+export let MaleCloth = 'https://res.cloudinary.com/dgqe5xli6/image/upload/v1721134601/IMG_20240716_183247_copy_2000x1333_tmy8uh.jpg';
 export let FemaleCloth = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838912/Cloths_kpwmp2.jpg';
-export let GroupCloth = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838911/Cloth-banner_ylavk9.jpg';
+export let GroupCloth = 'https://res.cloudinary.com/dgqe5xli6/image/upload/v1721131002/vdrw6987qgaow2n6ncrw.jpg';
 export let Electronics = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838911/Electronics_lrjvlg.jpg';
 export let Jewelry = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838911/model-jwellery_qlcjog.jpg';
 export let Shoes = 'https://res.cloudinary.com/dxguqzge7/image/upload/v1682838911/Shoe_e2yc1d.jpg';

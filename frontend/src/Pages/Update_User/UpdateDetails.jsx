6f05528d28fd7cee8608813baccf662e7096588f
tanjir238 <tanjir238@gmail.com>
2024-09-21 15:45:20 +0600

    update details

--- frontend/src/Pages/Update_User/UpdateDetails.jsx
+++ frontend/src/Pages/Update_User/UpdateDetails.jsx
@@ -172,45 +172,187 @@ const UpdateDetails = () => {
     }
     return (
         <>
-            <Container sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', marginBottom: 10 }}>
-                <Typography variant='h6' sx={{ margin: '30px 0', fontWeight: 'bold', color: '#1976d2' }}>Personal Information</Typography>
-                <form noValidate autoComplete="off" className={styles.checkout_form} onSubmit={handleSubmit} >
+            <Container 
+                sx={{
+                    width: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                    marginBottom: 10,
+                    padding: '20px',
+                }}>
+                <Typography 
+                    variant='h6' 
+                    sx={{
+                        margin: '30px 0',
+                        fontWeight: 'bold',
+                        color: '#1976d2',
+                        textAlign: 'center'
+                    }}>
+                    Personal Information
+                </Typography>
+    
+                <form 
+                    noValidate 
+                    autoComplete="off" 
+                    className={styles.checkout_form} 
+                    onSubmit={handleSubmit}
+                    style={{ width: '100%', maxWidth: '700px' }}>
                     <Grid container spacing={2}>
+                        {/* First Name */}
                         <Grid item xs={12} sm={6}>
-                            <TextField label="First Name" name='firstName' value={userDetails.firstName || ''} onChange={handleOnchange} variant="outlined" fullWidth />
+                            <TextField 
+                                label="First Name" 
+                                name='firstName' 
+                                value={userDetails.firstName || ''} 
+                                onChange={handleOnchange} 
+                                variant="outlined" 
+                                fullWidth 
+                                sx={{ borderRadius: '8px' }}
+                            />
                         </Grid>
+    
+                        {/* Last Name */}
                         <Grid item xs={12} sm={6}>
-                            <TextField label="Last Name" name='lastName' value={userDetails.lastName || ''} onChange={handleOnchange} variant="outlined" fullWidth />
+                            <TextField 
+                                label="Last Name" 
+                                name='lastName' 
+                                value={userDetails.lastName || ''} 
+                                onChange={handleOnchange} 
+                                variant="outlined" 
+                                fullWidth 
+                                sx={{ borderRadius: '8px' }}
+                            />
                         </Grid>
+    
+                        {/* Contact Number */}
                         <Grid item xs={12} sm={6}>
-                            <TextField label="Contact Number" type='tel' name='phoneNumber' value={userDetails.phoneNumber || ''} onChange={handleOnchange} variant="outlined" fullWidth />
+                            <TextField 
+                                label="Contact Number" 
+                                type='tel' 
+                                name='phoneNumber' 
+                                value={userDetails.phoneNumber || ''} 
+                                onChange={handleOnchange} 
+                                variant="outlined" 
+                                fullWidth 
+                                sx={{ borderRadius: '8px' }}
+                            />
                         </Grid>
+    
+                        {/* Email */}
                         <Grid item xs={12} sm={6}>
-                            <TextField label="Email" name='email' value={userDetails.email || ''} onChange={handleOnchange} variant="outlined" fullWidth />
+                            <TextField 
+                                label="Email" 
+                                name='email' 
+                                value={userDetails.email || ''} 
+                                onChange={handleOnchange} 
+                                variant="outlined" 
+                                fullWidth 
+                                sx={{ borderRadius: '8px' }}
+                            />
                         </Grid>
+    
+                        {/* Address */}
                         <Grid item xs={12}>
-                            <TextField label="Address" name='address' value={userDetails.address || ''} onChange={handleOnchange} variant="outlined" fullWidth />
+                            <TextField 
+                                label="Address" 
+                                name='address' 
+                                value={userDetails.address || ''} 
+                                onChange={handleOnchange} 
+                                variant="outlined" 
+                                fullWidth 
+                                sx={{ borderRadius: '8px' }}
+                            />
                         </Grid>
+    
+                        {/* City */}
                         <Grid item xs={12} sm={6}>
-                            <TextField label="City" name='city' value={userDetails.city || ''} onChange={handleOnchange} variant="outlined" fullWidth />
+                            <TextField 
+                                label="City" 
+                                name='city' 
+                                value={userDetails.city || ''} 
+                                onChange={handleOnchange} 
+                                variant="outlined" 
+                                fullWidth 
+                                sx={{ borderRadius: '8px' }}
+                            />
                         </Grid>
+    
+                        {/* Postal/Zip Code */}
                         <Grid item xs={12} sm={6}>
-                            <TextField type='tel' label="Postal/Zip Code" name='zipCode' value={userDetails.zipCode || ''} onChange={handleOnchange} variant="outlined" fullWidth />
+                            <TextField 
+                                type='tel' 
+                                label="Postal/Zip Code" 
+                                name='zipCode' 
+                                value={userDetails.zipCode || ''} 
+                                onChange={handleOnchange} 
+                                variant="outlined" 
+                                fullWidth 
+                                sx={{ borderRadius: '8px' }}
+                            />
                         </Grid>
-                        <Grid item xs={12} >
-                            <TextField label="Province/State" name='userState' value={userDetails.userState || ''} onChange={handleOnchange} variant="outlined" fullWidth />
+    
+                        {/* Province/State */}
+                        <Grid item xs={12}>
+                            <TextField 
+                                label="Province/State" 
+                                name='userState' 
+                                value={userDetails.userState || ''} 
+                                onChange={handleOnchange} 
+                                variant="outlined" 
+                                fullWidth 
+                                sx={{ borderRadius: '8px' }}
+                            />
                         </Grid>
                     </Grid>
-                    <Container sx={{ display: 'flex', justifyContent: 'space-around', marginTop: 5 }}>
-                        <Button variant='contained' endIcon={<TiArrowBackOutline />} onClick={()=>navigate(-1)} >Back</Button>
-                        <Button variant='contained' endIcon={<AiOutlineFileDone />}  type='submit'>Save</Button>
+    
+                    <Container 
+                        sx={{
+                            display: 'flex', 
+                            justifyContent: 'space-around', 
+                            marginTop: 5, 
+                            width: '100%'
+                        }}>
+                        <Button 
+                            variant='contained' 
+                            endIcon={<TiArrowBackOutline />} 
+                            onClick={() => navigate(-1)} 
+                            sx={{
+                                backgroundColor: '#1976d2',
+                                color: '#fff',
+                                '&:hover': { backgroundColor: '#1565c0' }
+                            }}>
+                            Back
+                        </Button>
+                        <Button 
+                            variant='contained' 
+                            endIcon={<AiOutlineFileDone />} 
+                            type='submit'
+                            sx={{
+                                backgroundColor: '#43a047',
+                                color: '#fff',
+                                '&:hover': { backgroundColor: '#388e3c' }
+                            }}>
+                            Save
+                        </Button>
                     </Container>
-                </form >
-
-                <Typography variant='h6' sx={{ margin: '20px 0', fontWeight: 'bold', color: '#1976d2' }}>Reset Password</Typography>
+                </form>
+    
+                {/* Reset Password Section */}
+                <Typography 
+                    variant='h6' 
+                    sx={{
+                        margin: '20px 0',
+                        fontWeight: 'bold',
+                        color: '#1976d2'
+                    }}>
+                    Reset Password
+                </Typography>
+    
                 <form onSubmit={handleResetPassword}>
                     <Grid container spacing={2}>
-                        <Grid item xs={12} >
+                        <Grid item xs={12}>
                             <TextField
                                 label="Current Password"
                                 name='currentPassword'
@@ -223,20 +365,18 @@ const UpdateDetails = () => {
                                     )
                                 }}
                                 value={password.currentPassword || ''}
-                                onChange={
-                                    (e) => setPassword({
-                                        ...password, [e.target.name]: e.target.value
-                                    })
-                                }
+                                onChange={(e) => setPassword({ ...password, [e.target.name]: e.target.value })}
                                 variant="outlined"
-                                fullWidth />
+                                fullWidth
+                                sx={{ borderRadius: '8px' }}
+                            />
                         </Grid>
-                        <Grid item xs={12} >
+    
+                        <Grid item xs={12}>
                             <TextField
                                 label="New Password"
                                 name='newPassword'
                                 type={showNewPassword ? "text" : "password"}
-                                id="password"
                                 InputProps={{
                                     endAdornment: (
                                         <InputAdornment position="end" onClick={() => setShowNewPassword(!showNewPassword)} sx={{ cursor: 'pointer' }}>
@@ -245,23 +385,47 @@ const UpdateDetails = () => {
                                     )
                                 }}
                                 value={password.newPassword || ''}
-                                onChange={
-                                    (e) => setPassword({
-                                        ...password, [e.target.name]: e.target.value
-                                    })
-                                }
+                                onChange={(e) => setPassword({ ...password, [e.target.name]: e.target.value })}
                                 variant="outlined"
-                                fullWidth />
+                                fullWidth
+                                sx={{ borderRadius: '8px' }}
+                            />
                         </Grid>
                     </Grid>
+    
                     <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: "25px 0", width: '100%' }}>
-                        <Button variant='contained' color='primary' endIcon={<RiLockPasswordLine />} type='submit'>Reset</Button>
+                        <Button 
+                            variant='contained' 
+                            color='primary' 
+                            endIcon={<RiLockPasswordLine />} 
+                            type='submit'
+                            sx={{
+                                backgroundColor: '#1976d2',
+                                color: '#fff',
+                                '&:hover': { backgroundColor: '#1565c0' }
+                            }}>
+                            Reset
+                        </Button>
                     </Box>
                 </form>
+    
+                {/* Delete Account Section */}
                 <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', margin: "25px 0", width: '100%' }}>
                     <Typography variant='h6'>Delete Your Account?</Typography>
-                    <Button variant='contained' color='error' endIcon={<AiFillDelete />} onClick={() => setOpenAlert(true)}>Delete</Button>
+                    <Button 
+                        variant='contained' 
+                        color='error' 
+                        endIcon={<AiFillDelete />} 
+                        onClick={() => setOpenAlert(true)}
+                        sx={{
+                            backgroundColor: '#d32f2f',
+                            '&:hover': { backgroundColor: '#c62828' }
+                        }}>
+                        Delete
+                    </Button>
                 </Box>
+    
+                {/* Confirmation Dialog */}
                 <Dialog
                     open={openAlert}
                     TransitionComponent={Transition}
@@ -269,22 +433,43 @@ const UpdateDetails = () => {
                     onClose={() => setOpenAlert(false)}
                     aria-describedby="alert-dialog-slide-description"
                 >
-                    {/* <DialogTitle>{"Use Google's location service?"}</DialogTitle> */}
                     <DialogContent sx={{ width: { xs: 280, md: 350, xl: 400 } }}>
                         <DialogContentText style={{ textAlign: 'center' }} id="alert-dialog-slide-description">
                             <Typography variant='body1'>Your all data will be erased</Typography>
                         </DialogContentText>
                     </DialogContent>
+    
                     <DialogActions sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
-                        <Button variant='contained' endIcon={<AiFillDelete />} color='error' onClick={deleteAccount}>Delete</Button>
-                        <Button variant='contained' color='primary'
-                            onClick={() => setOpenAlert(false)} endIcon={<AiFillCloseCircle />}>Close</Button>
+                        <Button 
+                            variant='contained' 
+                            endIcon={<AiFillDelete />} 
+                            color='error' 
+                            onClick={deleteAccount}
+                            sx={{
+                                backgroundColor: '#d32f2f',
+                                '&:hover': { backgroundColor: '#c62828' }
+                            }}>
+                            Delete
+                        </Button>
+                        <Button 
+                            variant='contained' 
+                            color='primary' 
+                            onClick={() => setOpenAlert(false)} 
+                            endIcon={<AiFillCloseCircle />}
+                            sx={{
+                                backgroundColor: '#1976d2',
+                                '&:hover': { backgroundColor: '#1565c0' }
+                            }}>
+                            Close
+                        </Button>
                     </DialogActions>
                 </Dialog>
-            </Container >
+            </Container>
+    
             <CopyRight sx={{ mt: 4, mb: 10 }} />
         </>
     )
+    
 }
 
 export default UpdateDetails

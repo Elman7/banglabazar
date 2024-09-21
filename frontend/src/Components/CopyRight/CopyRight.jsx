d5f16e1c2d4fea3ac01da615f9187993323a2918
tanjir238 <tanjir238@gmail.com>
2024-09-21 16:52:31 +0600

    facebook reference add

--- frontend/src/Components/CopyRight/CopyRight.jsx
+++ frontend/src/Components/CopyRight/CopyRight.jsx
@@ -1,19 +1,16 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { Typography } from '@mui/material';
+import React from 'react';
+import { FaFacebook } from 'react-icons/fa';
 
 const CopyRight = (props) => {
     return (
-        <a href='' target='_blank' rel='noreferrer' >
-
-            <Typography variant="body1" fontWeight="bold" color="text.secondary" align="center" {...props} style={{ color: '#1976d2',  }}>
-                {' '}
-                {new Date().getFullYear()}
-                {/* {'.'} */}
-                {' © '}
-               BANGLA BAZAAR
+        <a href='https://www.facebook.com/mdtanjir.rahman' target='_blank' rel='noreferrer' style={{ textDecoration: 'none', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <FaFacebook style={{ marginRight: '8px', color: '#1976d2' }} />
+            <Typography variant="body1" fontWeight="bold" color="text.secondary" align="center" {...props} style={{ color: '#1976d2' }}>
+                {new Date().getFullYear()} © BANGLA BAZAAR
             </Typography>
         </a>
-    )
+    );
 }
 
-export default CopyRight
+export default CopyRight;
